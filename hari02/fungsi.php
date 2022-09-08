dab667c59a9f6f66e0253ff5d91ad96aa497fc9f
Hariyanto Wibowo <hariwib@darmajaya.ac.id>
2022-09-08 20:44:18 +0700

    belajar oop

---
+++ hari02/fungsi.php
@@ -0,0 +1,13 @@
+<?php
+
+class Animal {
+  public $name;
+  public $legCount;
+  public $habitat;
+}
+
+$ayam = new Animal();
+$ayam->name = 'Ayam';
+$ayam->legCount = 2;
+$ayam->habitat = 'Darat';
+?>
